27127b25908e9b2cbc1445f2ad31b2cfb3de58a7
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2025-01-23 16:15:06 +0100

    add cfn lint

--- src/awsjavakit_cfn_rules/__init__.py
+++ src/awsjavakit_cfn_rules/__init__.py
@@ -1,13 +1,15 @@
-import os
+"""
+awsjavakit_cfn_rules package
+"""
 from pathlib import Path
-
-PROJECT_FOLDER = Path(os.path.abspath(__file__)).parent
-
+import os
 from awsjavakit_cfn_rules.rules.tags_rule import (
     TagsRule
 )
+
+PROJECT_FOLDER = Path(os.path.abspath(__file__)).parent
+
+
 __all__ = [
     "TagsRule"
 ]
-
-
--- src/awsjavakit_cfn_rules/rules/tags_rule.py
+++ src/awsjavakit_cfn_rules/rules/tags_rule.py
@@ -8,7 +8,9 @@ from cfnlint.template.template import Template
 
 EXPECTED_TAGS_FIELD_NAME = "expected_tags"
 
-CONFIG_DEFINITION = {EXPECTED_TAGS_FIELD_NAME: {"default": [], "type": "list", "itemtype": "string"}}
+CONFIG_DEFINITION = {
+    EXPECTED_TAGS_FIELD_NAME: {"default": [], "type": "list", "itemtype": "string"}
+}
 
 SAMPLE_TEMPLATE_RULE_ID = "E9001"
 
@@ -31,25 +33,28 @@ class TagsRule(CloudFormationLintRule):
     def match(self, cfn: Template) -> List[RuleMatch]:
         matches = []
         tags_rule_config = TagsRuleConfig(self.config)
-        for key, value in cfn.get_resources(["AWS::Lambda::Function"]).items():
-            tags: List[str] = self._extractTags_(value)
+        for _, value in cfn.get_resources(["AWS::Lambda::Function"]).items():
+            tags: List[str] = self._extract_tags_(value)
             missing_tags = self._calculate_missing_tags_(tags,tags_rule_config)
 
             if self._is_not_empty_(missing_tags):
                 matches.append(RuleMatch(path=["Resources", value],
-                                         message=f"Lambda Function is missing required tags:{str(missing_tags)}"))
+                                         message=self._construct_message_(missing_tags)))
         return matches
 
-    def _extractTags_(self, value) -> List[str]:
-        tagEntries = value.get("Properties").get("Tags")
-        tagNames = list(map(lambda tagEntry: tagEntry.get("Key"), tagEntries))
-        return tagNames
+    def _extract_tags_(self, value) -> List[str]:
+        tag_entries = value.get("Properties").get("Tags")
+        tag_names = list(map(lambda tagEntry: tagEntry.get("Key"), tag_entries))
+        return tag_names
 
     def _calculate_missing_tags_(self, tags: List[str], tags_rule_config: TagsRuleConfig) -> List[str]:
         return list(filter(lambda expected: (expected not in tags), tags_rule_config.expected_tags()))
 
     def _is_not_empty_(self, tags: List[str]) -> bool:
         return not (tags is None or tags == [])
+
+    def _construct_message_(self, missing_tags):
+        return f"Lambda Function is missing required tags:{str(missing_tags)}"
 
 
 @define
