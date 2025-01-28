32a79bd02d8d3c44cdec68a977a6510684a68d69
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2025-01-28 22:16:44 +0100

    tag_rule does the checks

--- src/awsjavakit_cfn_rules/rules/tags_checker.py
+++ src/awsjavakit_cfn_rules/rules/tags_checker.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, List
+from typing import Any, Iterable, List
 
 from attrs import define
 from cfnlint.rules import CloudFormationLintRule, RuleMatch
@@ -24,7 +24,7 @@ SAMPLE_TEMPLATE_RULE_ID = "E9001"
 EMPTY_DICT = {}
 
 
-class TagsRule(CloudFormationLintRule):
+class TagsChecker(CloudFormationLintRule):
 
     id: str = SAMPLE_TEMPLATE_RULE_ID
     shortdesc: str = "Missing Tags Rule for Resources"
@@ -44,8 +44,8 @@ class TagsRule(CloudFormationLintRule):
         resource_names = resources.keys()
         taggable_resources = filter(lambda resource_name: self._is_taggable_resource_(resources.get(resource_name)),
                                     resource_names)
-        matches = map(lambda resource_name: self._check_for_missing_tags_(cfn, resource_name, tags_rule_config),
-                      taggable_resources)
+        matches = list(map(lambda resource_name: self._check_for_missing_tags_(cfn, resource_name, tags_rule_config),
+                           taggable_resources))
 
         return self._flatten_(matches)
 
@@ -83,7 +83,7 @@ class TagsRule(CloudFormationLintRule):
     def _construct_message_(self, missing_tags, resource_name: str, resource_type: str) -> str:
         return f"Resource {resource_name}:{resource_type} is missing required tags:{str(missing_tags)}"
 
-    def _flatten_(self, matches: List[List[RuntimeError]]) -> List[RuleMatch]:
+    def _flatten_(self, matches: Iterable[List[RuntimeError]]) -> List[RuleMatch]:
         output = []
         for match in matches:
             output += match
@@ -96,3 +96,58 @@ class TagsRuleConfig:
 
     def expected_tags(self):
         return self.cfnlint_config.get(EXPECTED_TAGS_FIELD_NAME)
+
+
+@define
+class TagRule:
+    excluded_resource_types: List[str]
+    expected_tag: str
+
+    def validate_template(self, cfn: Template) -> List[RuleMatch]:
+
+        resources = cfn.get_resources()
+        resource_keys = resources.keys()
+        taggable_resources = filter(lambda key: self._is_taggable_resource_(resources.get(key)), resource_keys)
+        not_excluded_for_this_tag_rule = list(
+            filter(lambda key: self._is_not_excluded_(resources.get(key)), taggable_resources)
+        )
+
+        check_results = map(lambda key: self._calculate_missing_tags_(resource_name=key, resource=resources.get(key)),
+                            not_excluded_for_this_tag_rule)
+        non_none_check_results = filter(lambda result: result is not None, check_results)
+        matches = map(lambda check_result: check_result.as_rule_match(), non_none_check_results)
+        return list(matches)
+
+    def _is_taggable_resource_(self, resource: dict) -> bool:
+        return self._type_of_(resource) not in NON_TAGGABLE_RESOURCES
+
+    def _type_of_(self, resource: dict):
+        return resource.get("Type")
+
+    def _is_not_excluded_(self, resource: dict):
+        return self._type_of_(resource) not in self.excluded_resource_types
+
+    def _calculate_missing_tags_(self, resource_name: str, resource: dict) -> CheckResult | None:
+        if self.expected_tag not in self._extract_resource_tags(resource):
+            return CheckResult(resource=resource, missing_tag=self.expected_tag, resource_name=resource_name)
+        return None
+
+    def _extract_resource_tags(self, resource: dict) -> List[str]:
+        return resource.get("Properties", {}).get("Tags", [])
+
+
+@define
+class CheckResult:
+    resource: dict
+    resource_name: str
+    missing_tag: str
+
+    def as_rule_match(self) -> RuleMatch:
+        return RuleMatch(path=["Resources", self.resource],
+                         message=self._construct_message_())
+
+    def _construct_message_(self) -> str:
+        return f"Resource {self.resource_name}:{self._resource_type_()} is missing required tag:{self.missing_tag}"
+
+    def _resource_type_(self) -> str:
+        return self.resource.get("Type")
