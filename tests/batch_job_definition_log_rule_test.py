52db621c65d171173b8845288adc09e8d4f66909
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2025-12-18 21:29:26 +0100

    add two basic passing cases for job definitions

--- tests/batch_job_definition_log_rule_test.py
+++ tests/batch_job_definition_log_rule_test.py
@@ -24,6 +24,21 @@ class BatchJobDefinitionLogRuleTest:
         assert_that(results[0].linenumber).is_equal_to(resource_line_in_sample_file)
 
     @staticmethod
+    def should_accept_fargate_jobs_with_logging_config_that_contains_log_group():
+        template = TestUtils.parse_template(TEMPLATES / "batch_job_logging" / "passing" / "batch_job_definition.yaml")
+        results = BatchJobDefinitionLogRuleTest._run_template_(template)
+
+        assert_that(len(results)).described_as(str(results)).is_equal_to(0)
+
+    @staticmethod
+    def should_accept_fargate_jobs_with_logging_config_that_is_other_than_cloudwatch():
+        template = TestUtils.parse_template(TEMPLATES / "batch_job_logging" / "passing" \
+                                            /"batch_job_definition_with_other_than_cloudwatch.yaml")
+        results = BatchJobDefinitionLogRuleTest._run_template_(template)
+        assert_that(len(results)).described_as(str(results)).is_equal_to(0)
+
+
+    @staticmethod
     def _run_template_(resource: ParsedTemplate) -> list[Match]:
         configuration = ConfigMixIn(cli_args=None, **{"regions": ["eu-west-1"]})
         rules = TestUtils.load_all_rules()
